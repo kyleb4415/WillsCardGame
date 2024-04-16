613b6f9115bfc4c9f39ea86df7e719701ae017e5
kyleb4415 <helmskb@etsu.edu>
2024-04-16 13:54:49 -0400

    Created and started working on player game board, can click to place cards. .cs script is not functioning correctly.

--- Assets/Cards/CardData.gd
+++ Assets/Cards/CardData.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#written in godot for now because c# was having a fit over SQLite...
+
 @onready var card_size = self.size
 var card_name = "Burner"
 @onready var card_image_path = str("res://Assets/Cards/", card_name, ".png")
@@ -11,7 +13,20 @@ var db_name = "res://DataStore/CardData"
 func _ready():
 	db = SQLite.new()
 	db.path = db_name
-	var card_info = readDataFromDB()
+	
+	#used for dynamic card creation
+	var card_info = readDataForCard(card_name)
+	var card_name = str(card_info[0]["Name"])
+	var card_description = str(card_info[0]["Description"])
+	var card_attack = str(card_info[0]["Damage"])
+	var card_hp = str(card_info[0]["HP"])
+	var card_mana = str(card_info[0]["ManaCost"])
+	
+	$Bars/TopBar/Name/CenterContainer/Name.text = card_name
+	$Bars/TopBar/Cost/CenterContainer/Cost.text = card_mana
+	$Bars/CardDesc/DescContainer/CenterContainer/Desc.text = card_description
+	$Bars/BottomBar/AttackContainer/CenterContainer/Attack.text = card_attack
+	$Bars/BottomBar/HealthContainer/CenterContainer/Health.text = card_hp
 	
 	#resizing card to fit container
 	$Card.texture = load(card_image_path)
@@ -28,8 +43,10 @@ func commitDataToDB():
 	db.open_db()
 	var table_name = "Card"
 	var dict : Dictionary = Dictionary()
-	#finish implementation if need be... maybe only for changing unlock state
+	#implementation unfinished - may need to for unlock state
+	
 
+#test function, reads all rows from card table in db
 func readDataFromDB():
 	db.open_db()
 	var table_name = "Card"
@@ -37,4 +54,11 @@ func readDataFromDB():
 	for i in range(0, db.query_result.size()):
 		print("Query result ", db.query_result[i]["Name"], db.query_result[i]["Description"])
 		return db.query_result
+		
+#reads card by name
+func readDataForCard(card_name):
+	db.open_db()
+	db.query("SELECT * FROM CARD WHERE NAME = \"" + card_name + "\";")
+	print("Query result from single query ", db.query_result[0]["Name"], " ", db.query_result[0]["Description"])
+	return db.query_result
 
