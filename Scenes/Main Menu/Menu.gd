94fa7dfdd4af14af00745bda06a9696f51a8fa21
logan-stegmaier <114032249+logan-stegmaier@users.noreply.github.com>
2024-07-08 22:17:46 -0400

    Resolution Button Added
    Resolution button added - temporary placeholder. project settings changed

--- Scenes/Main Menu/Menu.gd
+++ Scenes/Main Menu/Menu.gd
@@ -1,14 +1,25 @@
 extends Control
 
-
-
 func _on_pressed():
 	get_tree().change_scene_to_file("res://Scenes3D/GameBoard.tscn") # Replace with function body.
 	
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/Options.tscn")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_item_selected(index):
+	match index: 
+		0: 
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1: 
+			DisplayServer.window_set_size(Vector2i(1600,900))	
+		2: 
+			DisplayServer.window_set_size(Vector2i(1280,720))	
+
+
+func back_on_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/MainMenu.tscn")
